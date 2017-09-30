62b827e861646e5e1296eacc396351628cc7072c
zhangxingang <zhangxingang@bfmj.com>
2017-09-30 17:30:12 +0800

    add swiper

--- pages/startup/startup.js
+++ pages/startup/startup.js
@@ -5,64 +5,100 @@ Page({
    * 页面的初始数据
    */
   data: {
-    mine:'我的圆桌',
+    title_image: '姓名.png',
+    mine: '我的圆桌',
     name: '李腾飞',
-    points:'60'
+    points: '60',
+    tabArr: {
+      curHdIndex: 0,
+      curBdIndex: 0
+    },
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换 
+    currentTab: 0,
+  },
+  bindSwiperChange: function (e) {
+
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+
+  },
+
+  swichNav: function (e) {
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
